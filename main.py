15ca38b6bdaf7e7db8f72844d85491b47ace182a
Joel <joelgerman671@gmail.com>
2024-02-05 01:06:31 -0400

    add new router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from data.db import  engine
 import models.models as models
 from api.endpoints.agency.agency_router import router as agency_router
+from api.endpoints.category.category_router import router as category_router
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,4 +10,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(agency_router)
+app.include_router(category_router)
 
