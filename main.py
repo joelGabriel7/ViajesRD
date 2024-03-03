d8746c5ce8609a1baccf8fb59c7e9dfdeb420133
Joel <joelgerman671@gmail.com>
2024-03-03 18:14:05 -0400

    restauring directory

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-app.mount("/static", StaticFiles(directory="static/images"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 app.include_router(category_router)
