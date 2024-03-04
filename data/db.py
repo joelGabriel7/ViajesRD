599b4a40783d79006cede4459135b6af5c8471f0
Joel <joelgerman671@gmail.com>
2024-03-04 10:52:59 -0400

    ..

--- data/db.py
+++ data/db.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import declarative_base
 import os
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:german2323@localhost/ViajeRD"
-# SQLALCHEMY_DATABASE_URL = "postgresql://viajesuser:9r6eTzoadNBtICB05iecXq0jjGv8FK6L@dpg-cnhk6ifsc6pc73dvpmmg-a.oregon-postgres.render.com/viajes_wyl5"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:german2323@localhost/ViajeRD"
+SQLALCHEMY_DATABASE_URL = "postgresql://viajesuser:9r6eTzoadNBtICB05iecXq0jjGv8FK6L@dpg-cnhk6ifsc6pc73dvpmmg-a.oregon-postgres.render.com/viajes_wyl5"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
