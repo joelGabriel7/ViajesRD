65cc1cf1406809f91f3bad4b501a2724bf333790
Joel German <joelgerman671@gmail.com>
2024-03-05 20:52:36 +0000

    updating files to new server

--- data/db.py
+++ data/db.py
@@ -5,7 +5,6 @@ import os
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:joel1234@localhost/viajes"
-#SQLALCHEMY_DATABASE_URL = "postgresql://viajesuser:9r6eTzoadNBtICB05iecXq0jjGv8FK6L@dpg-cnhk6ifsc6pc73dvpmmg-a.oregon-postgres.render.com/viajes_wyl5"
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
