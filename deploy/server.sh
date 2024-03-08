369cfce83cec3b3087f4b4a37867db704b66fa0d
Joel German <joelgerman671@gmail.com>
2024-03-08 20:56:42 +0000

    changes on server.sh

--- deploy/server.sh
+++ deploy/server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 FASTAPI_DIR=$(dirname $(cd `dirname $0` && pwd))
-
 cd $FASTAPI_DIR
-
 source env/bin/activate
-
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+exec uvicorn main:app --host 0.0.0.0  --port 8000
