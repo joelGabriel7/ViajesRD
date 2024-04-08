650dd314df35dd64ee18fbd5e5f921e24d854b71
Joel German <joelgerman671@gmail.com>
2024-04-08 18:36:45 +0000

    modified cors

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://www.apiviajesrd.info/"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
